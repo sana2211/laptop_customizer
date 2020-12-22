b9a2ba95e1d67827f3c6f6002f7d571cf20113e7
sana2211 <70413361+sana2211@users.noreply.github.com>
2020-12-22 01:08:20 -0600

    Create CartItem.js

---
+++ src/CartItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+
+class CartItem extends Component {
+
+    render() {
+
+        return (
+            <div className="summary__option" key={this.props.featureHash}>
+            <div className="summary__option__label">{this.props.feature} </div>
+            <div className="summary__option__value">{this.props.selectedOption}</div>
+            <div className="summary__option__cost">
+              {USCurrencyFormat.format(this.props.cost)}
+            </div>
+          </div>
+        );
+    }
+    
+
+}
+
+
+export default CartItem;
